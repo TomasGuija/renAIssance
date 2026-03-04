f6938e1d75cd470461e26dd845324f957a2faf0e
TomasGuija <tomasguija@gmail.com>
2026-03-04 18:15:42 +0100

    Dataset for our CSV format

--- src/dataset.py
+++ src/dataset.py
@@ -1,256 +1,134 @@
-import os
-import sys
 import re
-import six
+import csv
 import math
-import lmdb
 import torch
-
-from natsort import natsorted
+from pathlib import Path
 from PIL import Image
 import numpy as np
-from torch.utils.data import Dataset, ConcatDataset, Subset
-from torch._utils import _accumulate
+from torch.utils.data import Dataset
 import torchvision.transforms as transforms
 
 
-class Batch_Balanced_Dataset(object):
+class CsvLineDataset(Dataset):
 
-    def __init__(self, opt):
-        """
-        Modulate the data ratio in the batch.
-        For example, when select_data is "MJ-ST" and batch_ratio is "0.5-0.5",
-        the 50% of the batch is filled with MJ and the other 50% of the batch is filled with ST.
-        """
-        log = open(f'./saved_models/{opt.exp_name}/log_dataset.txt', 'a')
-        dashed_line = '-' * 80
-        print(dashed_line)
-        log.write(dashed_line + '\n')
-        print(f'dataset_root: {opt.train_data}\nopt.select_data: {opt.select_data}\nopt.batch_ratio: {opt.batch_ratio}')
-        log.write(f'dataset_root: {opt.train_data}\nopt.select_data: {opt.select_data}\nopt.batch_ratio: {opt.batch_ratio}\n')
-        assert len(opt.select_data) == len(opt.batch_ratio)
-
-        _AlignCollate = AlignCollate(imgH=opt.imgH, imgW=opt.imgW, keep_ratio_with_pad=opt.PAD)
-        self.data_loader_list = []
-        self.dataloader_iter_list = []
-        batch_size_list = []
-        Total_batch_size = 0
-        for selected_d, batch_ratio_d in zip(opt.select_data, opt.batch_ratio):
-            _batch_size = max(round(opt.batch_size * float(batch_ratio_d)), 1)
-            print(dashed_line)
-            log.write(dashed_line + '\n')
-            _dataset, _dataset_log = hierarchical_dataset(root=opt.train_data, opt=opt, select_data=[selected_d])
-            total_number_dataset = len(_dataset)
-            log.write(_dataset_log)
-
-            """
-            The total number of data can be modified with opt.total_data_usage_ratio.
-            ex) opt.total_data_usage_ratio = 1 indicates 100% usage, and 0.2 indicates 20% usage.
-            See 4.2 section in our paper.
-            """
-            number_dataset = int(total_number_dataset * float(opt.total_data_usage_ratio))
-            dataset_split = [number_dataset, total_number_dataset - number_dataset]
-            indices = range(total_number_dataset)
-            _dataset, _ = [Subset(_dataset, indices[offset - length:offset])
-                           for offset, length in zip(_accumulate(dataset_split), dataset_split)]
-            selected_d_log = f'num total samples of {selected_d}: {total_number_dataset} x {opt.total_data_usage_ratio} (total_data_usage_ratio) = {len(_dataset)}\n'
-            selected_d_log += f'num samples of {selected_d} per batch: {opt.batch_size} x {float(batch_ratio_d)} (batch_ratio) = {_batch_size}'
-            print(selected_d_log)
-            log.write(selected_d_log + '\n')
-            batch_size_list.append(str(_batch_size))
-            Total_batch_size += _batch_size
-
-            _data_loader = torch.utils.data.DataLoader(
-                _dataset, batch_size=_batch_size,
-                shuffle=True,
-                num_workers=int(opt.workers),
-                collate_fn=_AlignCollate, pin_memory=True)
-            self.data_loader_list.append(_data_loader)
-            self.dataloader_iter_list.append(iter(_data_loader))
-
-        Total_batch_size_log = f'{dashed_line}\n'
-        batch_size_sum = '+'.join(batch_size_list)
-        Total_batch_size_log += f'Total_batch_size: {batch_size_sum} = {Total_batch_size}\n'
-        Total_batch_size_log += f'{dashed_line}'
-        opt.batch_size = Total_batch_size
-
-        print(Total_batch_size_log)
-        log.write(Total_batch_size_log + '\n')
-        log.close()
-
-    def get_batch(self):
-        balanced_batch_images = []
-        balanced_batch_texts = []
-
-        for i, data_loader_iter in enumerate(self.dataloader_iter_list):
-            try:
-                image, text = next(data_loader_iter)
-                balanced_batch_images.append(image)
-                balanced_batch_texts += text
-            except StopIteration:
-                self.dataloader_iter_list[i] = iter(self.data_loader_list[i])
-                image, text = next(self.dataloader_iter_list[i])
-                balanced_batch_images.append(image)
-                balanced_batch_texts += text
-            except ValueError:
-                pass
-
-        balanced_batch_images = torch.cat(balanced_batch_images, 0)
-
-        return balanced_batch_images, balanced_batch_texts
-
-
-def hierarchical_dataset(root, opt, select_data='/'):
-    """ select_data='/' contains all sub-directory of root directory """
-    dataset_list = []
-    dataset_log = f'dataset_root:    {root}\t dataset: {select_data[0]}'
-    print(dataset_log)
-    dataset_log += '\n'
-    for dirpath, dirnames, filenames in os.walk(root+'/'):
-        if not dirnames:
-            select_flag = False
-            for selected_d in select_data:
-                if selected_d in dirpath:
-                    select_flag = True
-                    break
-
-            if select_flag:
-                dataset = LmdbDataset(dirpath, opt)
-                sub_dataset_log = f'sub-directory:\t/{os.path.relpath(dirpath, root)}\t num samples: {len(dataset)}'
-                print(sub_dataset_log)
-                dataset_log += f'{sub_dataset_log}\n'
-                dataset_list.append(dataset)
-
-    concatenated_dataset = ConcatDataset(dataset_list)
-
-    return concatenated_dataset, dataset_log
-
-
-class LmdbDataset(Dataset):
-
-    def __init__(self, root, opt):
-
-        self.root = root
+    def __init__(self, rows, opt, image_root):
         self.opt = opt
-        self.env = lmdb.open(root, max_readers=32, readonly=True, lock=False, readahead=False, meminit=False)
-        if not self.env:
-            print('cannot create lmdb from %s' % (root))
-            sys.exit(0)
+        self.image_root = Path(image_root).expanduser().resolve()
+        self.samples = []
+        out_of_char = f'[^{self.opt.character}]'
 
-        with self.env.begin(write=False) as txn:
-            nSamples = int(txn.get('num-samples'.encode()))
-            self.nSamples = nSamples
-
-            if self.opt.data_filtering_off:
-                # for fast check or benchmark evaluation with no filtering
-                self.filtered_index_list = [index + 1 for index in range(self.nSamples)]
-            else:
-                """ Filtering part
-                If you want to evaluate IC15-2077 & CUTE datasets which have special character labels,
-                use --data_filtering_off and only evaluate on alphabets and digits.
-                see https://github.com/clovaai/deep-text-recognition-benchmark/blob/6593928855fb7abb999a99f428b3e4477d4ae356/dataset.py#L190-L192
-
-                And if you want to evaluate them with the model trained with --sensitive option,
-                use --sensitive and --data_filtering_off,
-                see https://github.com/clovaai/deep-text-recognition-benchmark/blob/dff844874dbe9e0ec8c5a52a7bd08c7f20afe704/test.py#L137-L144
-                """
-                self.filtered_index_list = []
-                for index in range(self.nSamples):
-                    index += 1  # lmdb starts with 1
-                    label_key = 'label-%09d'.encode() % index
-                    label = txn.get(label_key).decode('utf-8')
-
-                    if len(label) > self.opt.batch_max_length:
-                        # print(f'The length of the label is longer than max_length: length
-                        # {len(label)}, {label} in dataset {self.root}')
-                        continue
-
-                    # By default, images containing characters which are not in opt.character are filtered.
-                    # You can add [UNK] token to `opt.character` in utils.py instead of this filtering.
-                    out_of_char = f'[^{self.opt.character}]'
-                    if re.search(out_of_char, label.lower()):
-                        continue
-
-                    self.filtered_index_list.append(index)
-
-                self.nSamples = len(self.filtered_index_list)
-
-    def __len__(self):
-        return self.nSamples
-
-    def __getitem__(self, index):
-        assert index <= len(self), 'index range error'
-        index = self.filtered_index_list[index]
-
-        with self.env.begin(write=False) as txn:
-            label_key = 'label-%09d'.encode() % index
-            label = txn.get(label_key).decode('utf-8')
-            img_key = 'image-%09d'.encode() % index
-            imgbuf = txn.get(img_key)
-
-            buf = six.BytesIO()
-            buf.write(imgbuf)
-            buf.seek(0)
-            try:
-                if self.opt.rgb:
-                    img = Image.open(buf).convert('RGB')  # for color image
-                else:
-                    img = Image.open(buf).convert('L')
-
-            except IOError:
-                print(f'Corrupted image for {index}')
-                # make dummy image and dummy label for corrupted image.
-                if self.opt.rgb:
-                    img = Image.new('RGB', (self.opt.imgW, self.opt.imgH))
-                else:
-                    img = Image.new('L', (self.opt.imgW, self.opt.imgH))
-                label = '[dummy_label]'
-
+        for row in rows:
+            raw_label = (row.get('gt_text') or '').strip()
             if not self.opt.sensitive:
-                label = label.lower()
+                raw_label = raw_label.lower()
+            label = re.sub(out_of_char, '', raw_label)
 
-            # We only train and evaluate on alphanumerics (or pre-defined character set in train.py)
-            out_of_char = f'[^{self.opt.character}]'
-            label = re.sub(out_of_char, '', label)
+            if not self.opt.data_filtering_off:
+                if len(label) == 0:
+                    continue
+                if len(label) > self.opt.batch_max_length:
+                    continue
 
-        return (img, label)
+            crop_rel = (row.get('crop_rel') or '').strip()
+            if not crop_rel:
+                continue
 
+            image_path = self.image_root / crop_rel
+            if not image_path.exists() and crop_rel.startswith('images/'):
+                image_path = self.image_root / crop_rel[len('images/'):]
 
-class RawDataset(Dataset):
-
-    def __init__(self, root, opt):
-        self.opt = opt
-        self.image_path_list = []
-        for dirpath, dirnames, filenames in os.walk(root):
-            for name in filenames:
-                _, ext = os.path.splitext(name)
-                ext = ext.lower()
-                if ext == '.jpg' or ext == '.jpeg' or ext == '.png':
-                    self.image_path_list.append(os.path.join(dirpath, name))
-
-        self.image_path_list = natsorted(self.image_path_list)
-        self.nSamples = len(self.image_path_list)
+            self.samples.append((image_path, label))
 
     def __len__(self):
-        return self.nSamples
+        return len(self.samples)
 
     def __getitem__(self, index):
-
+        image_path, label = self.samples[index]
         try:
             if self.opt.rgb:
-                img = Image.open(self.image_path_list[index]).convert('RGB')  # for color image
+                image = Image.open(image_path).convert('RGB')
             else:
-                img = Image.open(self.image_path_list[index]).convert('L')
-
-        except IOError:
-            print(f'Corrupted image for {index}')
-            # make dummy image and dummy label for corrupted image.
+                image = Image.open(image_path).convert('L')
+        except Exception:
             if self.opt.rgb:
-                img = Image.new('RGB', (self.opt.imgW, self.opt.imgH))
+                image = Image.new('RGB', (self.opt.imgW, self.opt.imgH))
             else:
-                img = Image.new('L', (self.opt.imgW, self.opt.imgH))
+                image = Image.new('L', (self.opt.imgW, self.opt.imgH))
+            label = '[dummy_label]'
 
-        return (img, self.image_path_list[index])
+        return image, label
+
+
+def _parse_index_string(index_string):
+    if not index_string:
+        return set()
+    values = set()
+    for token in index_string.split(','):
+        t = token.strip()
+        if not t:
+            continue
+        values.add(int(t))
+    return values
+
+
+def _read_dataset_csv(dataset_csv):
+    rows = []
+    with open(dataset_csv, 'r', newline='', encoding='utf-8') as handle:
+        reader = csv.DictReader(handle)
+        for row in reader:
+            if row.get('crop_rel') and row.get('gt_text') is not None and row.get('pdf_id'):
+                rows.append(row)
+    return rows
+
+
+def create_csv_split_datasets(dataset_csv, image_root, opt, val_pdf_indices='', test_pdf_indices=''):
+    rows = _read_dataset_csv(dataset_csv)
+    if not rows:
+        raise ValueError(f'No valid rows found in CSV: {dataset_csv}')
+
+    pdf_ids = sorted({row['pdf_id'] for row in rows})
+    val_idx_set = _parse_index_string(val_pdf_indices)
+    test_idx_set = _parse_index_string(test_pdf_indices)
+
+    overlap = val_idx_set.intersection(test_idx_set)
+    if overlap:
+        raise ValueError(f'Validation and test indices overlap: {sorted(overlap)}')
+
+    max_idx = len(pdf_ids) - 1
+    for idx in sorted(val_idx_set.union(test_idx_set)):
+        if idx < 0 or idx > max_idx:
+            raise ValueError(f'PDF index out of range: {idx}. Valid range is 0..{max_idx}')
+
+    val_pdf_ids = {pdf_ids[idx] for idx in val_idx_set}
+    test_pdf_ids = {pdf_ids[idx] for idx in test_idx_set}
+
+    train_rows = []
+    val_rows = []
+    test_rows = []
+
+    for row in rows:
+        pdf_id = row['pdf_id']
+        if pdf_id in val_pdf_ids:
+            val_rows.append(row)
+        elif pdf_id in test_pdf_ids:
+            test_rows.append(row)
+        else:
+            train_rows.append(row)
+
+    train_dataset = CsvLineDataset(train_rows, opt=opt, image_root=image_root)
+    val_dataset = CsvLineDataset(val_rows, opt=opt, image_root=image_root)
+    test_dataset = CsvLineDataset(test_rows, opt=opt, image_root=image_root)
+
+    split_info = {
+        'pdf_ids': pdf_ids,
+        'val_pdf_ids': sorted(val_pdf_ids),
+        'test_pdf_ids': sorted(test_pdf_ids),
+        'train_count': len(train_dataset),
+        'val_count': len(val_dataset),
+        'test_count': len(test_dataset),
+    }
+
+    return train_dataset, val_dataset, test_dataset, split_info
 
 
 class ResizeNormalize(object):
@@ -324,16 +202,3 @@ class AlignCollate(object):
             image_tensors = torch.cat([t.unsqueeze(0) for t in image_tensors], 0)
 
         return image_tensors, labels
-
-
-def tensor2im(image_tensor, imtype=np.uint8):
-    image_numpy = image_tensor.cpu().float().numpy()
-    if image_numpy.shape[0] == 1:
-        image_numpy = np.tile(image_numpy, (3, 1, 1))
-    image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
-    return image_numpy.astype(imtype)
-
-
-def save_image(image_numpy, image_path):
-    image_pil = Image.fromarray(image_numpy)
-    image_pil.save(image_path)
